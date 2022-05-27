1f5d99ebbcc86e7c212cb342006890f406a3cef9
Samarth <samarth@latlong.ai>
2022-05-27 11:31:03 +0530

    added routes and created controllers, database and helper packages

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,9 @@
 package routes
+
+import "github.com/gin-gonic/gin"
+
+
+func AuthRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.POST("users/signup",controller.Signup())
+	incomingRoutes.POST("users/login",controller.Login())
+}
