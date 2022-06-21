08e2af867e6e22b6026c4d3782126f7645b54854
Samarth <samarthaasoori@gmail.com>
2022-06-21 16:17:24 +0530

    added get all users endpoint logic

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,13 +2,13 @@ package controllers
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/Samarth2898/golangjwt/database"
-	"github.com/Samarth2898/golangjwt/helpers"
+	helper "github.com/Samarth2898/golangjwt/helpers"
 	"github.com/Samarth2898/golangjwt/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -22,7 +22,13 @@ import (
 var userCollection *mongo.Collection = database.OpenCollection(database.Client,"user")
 var validate = validator.New()
 
-func HashPassword()
+func HashPassword(password string) string{
+	pass, err := bcrypt.GenerateFromPassword([]byte(password),14)
+	if err!=nil{
+		log.Panic(err.Error())
+	}
+	return string(pass)
+}
 
 
 func VerifyPassword(userPassword string, providedPassword string)(bool, string){
@@ -31,7 +37,7 @@ func VerifyPassword(userPassword string, providedPassword string)(bool, string){
 	msg := ""
 
 	if err!=nil{
-		msg = fmt.Sprint("email or password is incorrect")
+		msg = "email or password is incorrect"
 		check = false
 	}
 	return check, msg
@@ -61,6 +67,9 @@ func Signup() gin.HandlerFunc{
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while checking for email"})
 			return
 		}
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
 
 		phoneCount, err := userCollection.CountDocuments(dctx, bson.M{"phone":user.Phone})
 		defer cancel()
@@ -79,13 +88,13 @@ func Signup() gin.HandlerFunc{
 		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		user.ID = primitive.NewObjectID()
 		user.User_id = user.ID.Hex()
-		token, refreshToken, _ := helpers.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, *&user.User_id)
+		token, refreshToken, _ := helper.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, user.User_id)
 		user.Token = &token
 		user.Refresh_token = &refreshToken
 
 		resultInsertionNumber, insertError :=userCollection.InsertOne(dctx, user)
 		if insertError!=nil{
-			msg := fmt.Sprintf("user item was not created")
+			msg := "user item was not created"
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error":msg})
 			return
 		}
@@ -116,20 +125,75 @@ func Login() gin.HandlerFunc{
 		}
 
 		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
-		defer cancel()
+		if !passwordIsValid {
+			ctx.JSON(http.StatusInternalServerError,gin.H{"error":msg})
+			return
+		}
 
+		if foundUser.Email == nil{
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error":"user not found"})
+		}
 
+		token, refreshToken, _ := helper.GenerateAllTokens(*foundUser.Email,*foundUser.First_name,*foundUser.Last_name,*foundUser.User_type,foundUser.User_id)
+		helper.UpdateAllTokens(token, refreshToken, foundUser.User_id)
+
+		err = userCollection.FindOne(dctx, bson.M{"user_id": foundUser.User_id}).Decode(&foundUser)
+		if err!=nil{
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, foundUser)
 	}
 }
 
-func GetUser(){
-
-}
-
 func GetUsers() gin.HandlerFunc{
+	return func(c *gin.Context){
+		if err := helper.CheckUserType(c, "ADMIN"); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err != nil || recordPerPage <1{
+			recordPerPage = 10
+		}
+		page, err1 := strconv.Atoi(c.Query("page"))
+		if err1 !=nil || page<1{
+			page = 1
+		}
+
+		startIndex := (page - 1) * recordPerPage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{primitive.E{Key:"$match", Value:bson.D{{}}}}
+		groupStage := bson.D{primitive.E{Key:"$group",Value:bson.D{
+			primitive.E{Key:"_id", Value:bson.D{primitive.E{Key:"_id", Value: "null"}}}, 
+			primitive.E{Key:"total_count", Value: bson.D{primitive.E{Key:"$sum", Value: 1}}}, 
+			primitive.E{Key:"data", Value: bson.D{primitive.E{Key:"$push", Value: "$$ROOT"}}}}}}
+		projectStage := bson.D{
+			{Key:"$project",Value:  bson.D{
+				{Key:"_id", Value:  0},
+				{Key:"total_count", Value: 1},
+				{Key:"user_items", Value:bson.D{{Key:"$slice", Value:[]interface{}{"$data", startIndex, recordPerPage}}}},}}}
+		result,err := userCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, groupStage, projectStage})
+		defer cancel()
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while listing user items"})
+		}
+		var allusers []bson.M
+		if err = result.All(ctx, &allusers); err!=nil{
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allusers[0])
+}}
+
+func GetUser() gin.HandlerFunc{
 	return func(ctx *gin.Context) {
 		user_id := ctx.Param("user_id")
-		if err:= helpers.MatchUserTypeToUid(ctx, user_id); err!=nil{
+		if err:= helper.MatchUserTypeToUid(ctx, user_id); err!=nil{
 			ctx.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
 			return
 		}
--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,11 +1,17 @@
 package helpers
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
 
+	"github.com/Samarth2898/golangjwt/database"
 	jwt "github.com/golang-jwt/jwt/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct{
@@ -17,7 +23,7 @@ type SignedDetails struct{
 	jwt.StandardClaims
 }
 
-// var userCollection *mongo.Collection = database.OpenCollection(database.Client,"user")
+var userCollection *mongo.Collection = database.OpenCollection(database.Client,"user")
 
 var SECRET_KEY = os.Getenv("SECRET_KEY")
 
@@ -52,3 +58,36 @@ func GenerateAllTokens(email string, firstName string, lastName string, userType
 	}
 	return
 }	
+
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userID string){
+	ctx, cancel := context.WithTimeout(context.Background(),100*time.Second)
+	var updateObj primitive.D
+
+	updateObj = append(updateObj, bson.E{Key:"token",Value:signedToken})
+	updateObj = append(updateObj, bson.E{Key:"refreshToken", Value:signedRefreshToken})
+
+	Updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj = append(updateObj, bson.E{Key:"updated_at", Value:Updated_at})
+
+	upsert := true
+	filter := bson.M{"user_id": userID}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+
+	_, err := userCollection.UpdateOne(
+		ctx,
+		filter,
+		bson.D{
+			{Key:"$set", Value:updateObj},
+		},
+		&opt,
+	) 
+
+	defer cancel()
+	if err!=nil{
+		log.Panic(err.Error())
+		return
+	}
+	
+}
